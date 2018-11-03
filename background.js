6d419433687e62db5cbffff546f515ada37cec26
Jonathan Serra <jonathan@cryptoblocs.fr>
2018-11-03 23:41:09 +0100

    Init commit

---
+++ background.js
@@ -0,0 +1,48 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+function getTrustedURI() {
+  const trustedEndpoints = [
+    'https://cryptoblocs.fr/assets/scamscan.json',
+  ];
+
+  return trustedEndpoints[Math.floor(Math.random() * Math.floor(trustedEndpoints.length - 1))];
+}
+
+// Updates the list everytime the user opens Chrome
+function updateList() {
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+
+  xhr.open('GET', getTrustedURI(), true);
+  xhr.onload = function(e) {
+    chrome.storage.sync.set({scamscan: this.response});
+  };
+  xhr.send();
+}
+
+
+chrome.windows.onCreated.addListener(function() {
+  updateList();
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+  updateList();
+
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    chrome.storage.sync.get('scamscan', function(data) {
+      const url = new URL(tab.url);
+
+      const scam = data.scamscan[url.hostname] ||
+        // Checks without subdomain
+        data.scamscan[url.hostname.split('.').slice(1).join('.')];
+
+      if (scam) {
+        chrome.tabs.sendMessage(tabId, {scam, id: tabId});
+      }
+    });
+  });
+});
---
+++ scam.js
@@ -0,0 +1,57 @@
+'use strict';
+
+function displayRecommendations(recommendations) {
+  if (recommendations && recommendations.length > 0) {
+    let res = '<p>Voici nos recommendations proposant un service similaire à ce que vous cherchez : <ul>';
+    recommendations.map((reco) => res += `<li><a href="${reco}">${reco}</a></li>`);
+    return res + '</ul>';
+  }
+  return '';
+}
+
+chrome.runtime.onMessage.addListener((request, sender, callback) => {
+  const scam = request.scam;
+  if (scam) {
+    window.stop();
+
+    const head = document.head || document.getElementsByTagName('head')[0];
+    let style = document.createElement('style');
+
+    style.type = 'text/css';
+    const css = `
+      #${scam.id} {
+        display: 'block' !important;
+        padding: 25px;
+      }
+      #${scam.id} * {
+        font-family: Arial, Helvetica, sans-serif;
+      }
+      #${scam.id} h1 {
+        font-weight: 400;
+        margin-bottom: 10px;
+        padding: 0;
+      }
+    `;
+
+    if (style.styleSheet){
+      // IE
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+
+    head.appendChild(style);
+
+    document.body.innerHTML = `
+      <div id=${scam.id}>
+        <h1>Attention ! Ce site est listé comme étant frauduleux</h1>
+
+        <p>
+          Ce site est listé sur <a href="${scam.source}">${scam.source}</a>
+        </p>
+
+        ${displayRecommendations(scam.recommendations)}
+      </div>
+    `;
+  }
+});
